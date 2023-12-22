f031c4af53f51f15594168d386e94ed517f6fd51
André Ramos <andreluis0703@gmail.com>
2023-12-21 21:36:30 -0300

    Update funcoes.js

--- Introducao_JS/funcoes.js
+++ Introducao_JS/funcoes.js
@@ -1,9 +1,11 @@
 
 
 function saudacoes() {
+    console.log('Oi')
     console.log('Olá, bom dia!')
     console.log('Boa tarde, como vai?')
     console.log('Boa noite e bom descanço!')
+    console.log('Tchau')
 }
 
 saudacoes()
