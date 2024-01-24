8fa10e83ef340d0390b6d1ed23aaf08f08b64b80
André Ramos <andreluis0703@gmail.com>
2024-01-23 23:45:36 -0300

    feat: manipulacao de casas decimais e textos

---
+++ Introducao_JS/manipulacoes.js
@@ -0,0 +1,9 @@
+let number = 345.1425325;
+
+console.log(number.toFixed(2).replace(".",",")) // A funcção toFixed resume o número após a virgula para 2 casas decimais.
+                                                // E a função replace troca um valor por outro do que será exibido no console log.
+
+let frase = 'Estudando JavaScript'
+
+console.log(frase.toUpperCase()) // A funcção toUpperCase transforma o texto todo em maiusculo.
+console.log(frase.toLowerCase()) // E a função toLowerCase transforma o texto todo em minusculo.
